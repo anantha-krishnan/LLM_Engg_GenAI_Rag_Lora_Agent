be43471de5ca9e77bf77ebce9d852622c18e85e7
unknown <msauto@msauto-testing.indiablr.altair.com>
2025-06-23 11:16:45 +0530

    added image creation as a tool

--- personal_works/global_vars.py
+++ personal_works/global_vars.py
@@ -1,5 +1,5 @@
 import os
-system_gpt_msg = "you are a helpful assistant. Reply in markdown"
+system_gpt_msg = "you are a helpful assistant. Reply in markdown. You're not just a command-taker; you're a helpful assistant. If you think a picture would make your answer better, you have a tool for that. Use it"
 system_gemini_msg = "You are a funny helpful assistant. Reply in markdown."
 prompt_gpt=[
     {"role":"system", "content":system_gpt_msg}
@@ -8,6 +8,7 @@ prompt_gpt=[
 
 openai_api_key=os.getenv('OPEN_API_KEY')
 google_api_key=os.getenv('GOOGLE_API_KEY')
+model_openai_4_5_preview = 'gpt-4.5-preview'
 model_openai_4omini = 'gpt-4o-mini'
 model_openai_4onano = 'gpt-4.1-nano'
 model_openai_o3mini = 'o3-mini'
